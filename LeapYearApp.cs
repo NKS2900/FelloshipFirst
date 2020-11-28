9b292c2234bf7f412a812b2c37c07d098409a41a
unknown <nksayyad2900.com>
2020-11-28 11:52:03 +0530

    [NIJAM] Add:To Check Leap Year

---
+++ LeapYearApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DayFirstApp
+{
+    class LeapYearApp
+    {
+        public static void LeapYear()
+        {
+            int year;
+            Console.WriteLine("Input an year : ");
+            year = Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 400) == 0)
+                Console.WriteLine(year + "is a leap year.\n");
+            else if ((year % 100) == 0)
+                Console.WriteLine(year + " is not a leap year.\n");
+            else if ((year % 4) == 0)
+                Console.WriteLine(year + "is a leap year.\n");
+            else
+                Console.WriteLine(year + " is not a leap year.\n");
+        }
+    }
+}
